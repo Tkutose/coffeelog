4a742aa325544f181d107b20cc03631a2fb22db8
kucch <aderybase910@gmail.com>
2021-07-16 13:57:52 +0900

    heroku app first commit

--- testsite/settings.py
+++ testsite/settings.py
@@ -137,12 +137,3 @@ DATABASES['default'].update(db_from_env)
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DEBUG = True
