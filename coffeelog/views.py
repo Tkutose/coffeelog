a7ebefb26c71977d92b5479b4fe949edbf74b0f0
kucch <aderybase910@gmail.com>
2021-07-29 11:20:04 +0900

    listの作成

--- coffeelog/views.py
+++ coffeelog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Log
+from .models import Log, Store
 
 # Create your views here.
 
@@ -8,10 +8,22 @@ class LogDetail(generic.DetailView):
     model = Log
     context_object_name = 'log'
 
-
+    # 選択した商品と同じ店舗の商品を取得
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         obj = context.get('object')
         log = Log.objects.filter(store__store=obj.store).exclude(id=obj.id)
         context['other'] = "まだ登録されていませんでした" if not log else log
+        return context
+
+
+class LogList(generic.ListView):
+    model = Log
+    context_object_name = 'log_list'
+
+
+    # 選択した商品と同じ店舗の商品を取得
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stores'] = Store.objects.all()
         return context
