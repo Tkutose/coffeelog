c3402dc05c5f0d939ff758b59e3f72e327e4cb72
kucch <aderybase910@gmail.com>
2021-08-05 19:13:52 +0900

    デプロイ用設定

--- testsite/settings.py
+++ testsite/settings.py
@@ -27,8 +27,6 @@ except ImportError:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ayuyopg6c_t#abq9_cmc@nogy@n-)kw+u!$a-ma**ksvex7ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
